a7e710ff3e8ed99cf11d00b4cbcd1108ddedf5f7
juscing <juscing@gmail.com>
2012-11-26 20:22:56 -0500

    Adding the JUnit stub for the Helper class

---
+++ Grounds Tour/src/com/mixd/grounds/tour/HelperTest.java
@@ -0,0 +1,34 @@
+package com.mixd.grounds.tour;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelperTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testLatLngDistDoubleDoubleDoubleDouble() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testLatLngDistLocationLocation() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testLatLngBearingDeg() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testLatLngBearingRad() {
+        fail("Not yet implemented");
+    }
+
+}
